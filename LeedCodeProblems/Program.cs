eff1e306b9a76d86f0f8b9d6c97131db170de7ba
Kirill Lushnikov <lushnikovkf@yandex.ru>
2024-07-17 00:40:15 +0300

    Add project files.

---
+++ LeedCodeProblems/Program.cs
@@ -0,0 +1,32 @@
+﻿using BenchmarkDotNet.Attributes;
+
+
+//BenchmarkDotNet.Running.BenchmarkRunner.Run<Test.TestRun>();
+
+namespace LeedCodeProblems
+{
+    [MemoryDiagnoser]
+    public class BenchmarkRun()
+    {
+        [Benchmark]
+        public void Test1()
+        {
+            Solution solution = new();
+        }
+    }
+
+
+    public class Run
+    {
+        public static void Main()
+        {
+            Solution solution = new();
+        }
+    }
+
+
+    public class Solution
+    {
+
+    }
+}
---
+++ LeedCodeProblems/TreeNode.cs
@@ -0,0 +1,49 @@
+﻿namespace LeedCodeProblems
+{
+    public class TreeNode(int val = 0, TreeNode? left = null, TreeNode? right = null)
+    {
+        public int val = val;
+        public TreeNode? left = left;
+        public TreeNode? right = right;
+
+        private static List<int> countLvl = [];
+
+        public static TreeNode? CreateToArray(List<int?> arr, int deep = 0)
+        {
+            if (countLvl.Count == deep)
+            {
+                countLvl.Add(1);
+            }
+
+            int index = (int)Math.Pow(2, deep) - countLvl[deep];
+
+            if (index >= arr.Count)
+            {
+                return null;
+            }
+
+            if (arr[index] == null)
+            {
+                return null;
+            }
+
+            TreeNode tn = new();
+            tn.val = arr[index]!.Value;
+
+            tn.left = CreateToArray(arr, deep + 1);
+            tn.right = CreateToArray(arr, deep + 1);
+
+            if (tn.left != null && tn.right != null)
+            {
+                for (int i = deep + 1; i < countLvl.Count; i++)
+                {
+                    countLvl[i]++;
+                }
+            }
+
+            arr.RemoveAt(index);
+
+            return tn;
+        }
+    }
+}
