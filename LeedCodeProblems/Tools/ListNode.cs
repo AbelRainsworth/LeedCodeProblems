b987d78d4e362f4292bd5dd19f2406c985bda74c
Kirill Lushnikov <lushnikovkf@yandex.ru>
2024-07-28 22:33:31 +0300

    Add a method that creates a listnode from an array.

--- LeedCodeProblems/Tools/ListNode.cs
+++ LeedCodeProblems/Tools/ListNode.cs
@@ -4,5 +4,21 @@
     {
         public int val = val;
         public ListNode? next = next;
+
+        public static ListNode? CreateFromArray(List<int> arr, int deep = 0)
+        {
+            if (arr.Count == deep)
+            {
+                return null;
+            }
+
+            ListNode result = new()
+            {
+                val = arr[deep],
+                next = CreateFromArray(arr, deep + 1)
+            };
+
+            return result;
+        }
     }
 }
