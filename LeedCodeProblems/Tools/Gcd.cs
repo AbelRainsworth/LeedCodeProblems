2766f07fa272a41669a0b751343e4d54c4f94f5d
Kirill Lushnikov <lushnikovkf@yandex.ru>
2024-08-23 13:11:35 +0300

    Add binary Gcd.

---
+++ LeedCodeProblems/Tools/Gcd.cs
@@ -0,0 +1,46 @@
+﻿namespace LeedCodeProblems.Tools
+{
+    public class Gcd
+    {
+        public static int BinaryGcd(int a, int b)
+        {
+            int shift;
+            if (a == 0)
+            {
+                return b;
+            }
+
+            if (b == 0)
+            {
+                return a;
+            }
+
+            for (shift = 0; ((a | b) & 1) == 0; ++shift)
+            {
+                a >>= 1;
+                b >>= 1;
+            }
+            while ((a & 1) == 0)
+            {
+                a >>= 1;
+            }
+
+            do
+            {
+                while ((b & 1) == 0)
+                {
+                    b >>= 1;
+                }
+
+                if (a > b)
+                {
+                    (a, b) = (b, a);
+                }
+                b = b - a;
+            } while (b != 0);
+
+            return a << shift;
+        }
+    }
+}
+
